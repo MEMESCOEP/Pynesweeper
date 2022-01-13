9fee8bf19ee70c1488f7afc3e5560fbe8d122ff8
Andrew Maney <67339620+MEMESCOEP@users.noreply.github.com>
2022-01-12 21:33:25 -0500

    Add batch file to install dependencies

---
+++ install_dependencies.bat
@@ -0,0 +1,19 @@
+@echo off
+cls
+echo Installing Dependencies...
+echo Installing pyinstaller
+pip install pyinstaller
+
+echo Installing numpy
+pip install numpy
+
+echo Installing PyQt5
+pip install PyQt5
+
+echo Installing itertools
+pip install itertools
+
+echo Installing functools
+pip install functools
+
+echo Done.
